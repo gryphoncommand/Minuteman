a1e6d7031953c24fbd4f959aca299053dc16925f
RotomReploid <jamugemuzi@gmail.com>
2025-10-27 16:04:26 -0400

    Task "make this a spark flex because carson hates me :(" DONE

--- src/main/java/frc/robot/subsystems/Spindexer/Passthrough.java
+++ src/main/java/frc/robot/subsystems/Spindexer/Passthrough.java
@@ -1,37 +1,37 @@
 package frc.robot.subsystems.Spindexer;
 
-import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.signals.NeutralModeValue;
-import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Configs;
 import frc.robot.Constants.SpindexerConstants;
 
-
-// TODO: make this a spark flex because carson hates me :(
 public class Passthrough extends SubsystemBase {
-    private final TalonFX passthroughMotor = new TalonFX(SpindexerConstants.kPassthroughMotorID);
-    private final DutyCycleOut openLoopRequest = new DutyCycleOut(0);
+    private final SparkFlex passthroughMotor = new SparkFlex(SpindexerConstants.kPassthroughMotorID, MotorType.kBrushless);
 
     public Passthrough() {
-        passthroughMotor.setNeutralMode(NeutralModeValue.Brake);
-        passthroughMotor.setPosition(0);
+        passthroughMotor.configure(Configs.SubsystemBaseConfig.subsystemConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+        passthroughMotor.getClosedLoopController().setReference(0, ControlType.kPosition);
     }
 
     @Override
     public void periodic() {
-        SmartDashboard.putNumber("Passthrough Pos (rot)", passthroughMotor.getPosition().getValueAsDouble());
-        SmartDashboard.putNumber("Passthrough Current (A)", passthroughMotor.getSupplyCurrent().getValueAsDouble());
+        SmartDashboard.putNumber("Passthrough Pos (rot)", passthroughMotor.getEncoder().getPosition());
+        SmartDashboard.putNumber("Passthrough Current (A)", passthroughMotor.getOutputCurrent());
     }
 
     /** Sets the passthrough motor speed (-1.0 to 1.0). */
     public void setSpeed(double percentOutput) {
-        passthroughMotor.setControl(openLoopRequest.withOutput(percentOutput));
+        passthroughMotor.set(percentOutput);
     }
 
     /** Stops the passthrough motor. */
     public void stop() {
-        passthroughMotor.setControl(openLoopRequest.withOutput(0));
+        passthroughMotor.set(0);
     }
 }
